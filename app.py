3ddfc0e90733720182edf0832179b79ba8a6d9a0
Gaëtan H. <gaetan.hrlt@gmail.com>
2024-12-17 17:26:56 +0100

    integrate our service to app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from models.database import db
 from routes.user_routes import user_routes
 from routes.invoice_routes import invoice_routes
+from routes.product_routes import product_routes
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +16,8 @@ def create_app():
     # Register blueprints
     app.register_blueprint(user_routes, url_prefix="/api")
     app.register_blueprint(invoice_routes, url_prefix="/api")
+    app.register_blueprint(product_routes, url_prefix="/api")
+
 
 
     return app
